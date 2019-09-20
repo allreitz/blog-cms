8f404d87de456fbde4cb6d14ebb4dfb7d9ba97bd
Alex Reitz <alexander.w.reitz@gmail.com>
2019-09-20 15:16:18 -0400

    fix error

--- header.php
+++ header.php
@@ -5,5 +5,5 @@
     <link rel="stylesheet" type="text/css" href="format.css" />
     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
 
-  </head>
+</head>
  
