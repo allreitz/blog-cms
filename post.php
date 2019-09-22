dcdda574f17aa92d5c491d8a69ec507a34adba32
Alex Reitz <alexander.w.reitz@gmail.com>
2019-09-22 13:24:22 -0400

    fixing css and finalizing submit button

--- post.php
+++ post.php
@@ -5,9 +5,26 @@
     include "nav.php";
   ?>
   <main>
-              
-    <h1>Lorem Ipsum</h1>
-  
+  <?php
+    function getPostDetailsFromDatabase() {
+      $postDetails = array('title' => 'Blog Post 1',
+                          'content' => 'my first blog post',
+                          'date' => '01/01/2020',
+                          'author' => 'alexanderreitz');
+      return $postDetails;
+    }      
+  ?>
+  <?php
+      //post details contains all the  data  to generate the data from
+      $postDetails = getPostDetailsFromDatabase();
+  ?>
+  <h1> <?php echo $postDetails["title"]; ?> </h1>
+  <div> <?php echo $postDetails["author"]; ?> </div>
+  <div> <?php echo $postDetails["date"]; ?> </div>
+  <div> <?php echo $postDetails["content"]; ?> </div>
+   <h1>Lorem Ipsum</h1>
+    echo "<li><a href=='post.php?title=" . $postTitle . "'>" . $postTitle .
+    "</a></li>";
     <article>
         <div style="background-color:lightblue">
             <h3>Author: Alexander Reitz</h3>
@@ -21,6 +38,8 @@
                  deserunt mollit anim id est laborum."</p>
         </div>
     </article>
+
+   
   </main>
   <?php
     include "footer.php";
